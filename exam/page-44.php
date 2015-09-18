4c8df454694ddb31d4fdb450f7987200f82ed818
lupaxa <lupaxa87@gmail.com>
2015-09-18 13:30:31 +0300

    no message

---
+++ exam/page-44.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>My project</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style-5.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+ <header>
+ 	<div class="header-wrap wrap">
+ 		<div class="logo-block">
+	 		<div class="logo"><img src="<?php echo get_template_directory_uri(); ?>/img/logo1.png"></div>
+	 		<p>abc university</p>
+	 	</div>
+ 		<div class="social">
+			<div class="social-title">connect</div>
+			<div class="social-logo">
+	 			<div class="social-img"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" alt=""></div>
+	 			<div class="social-img"><img src="<?php echo get_template_directory_uri(); ?>/img/gplus.png"  alt=""></div>
+	 			<div class="social-img"><img src="<?php echo get_template_directory_uri(); ?>/img/tw.png"  alt=""></div>
+ 			</div>
+ 		</div>
+ 	</div>
+ </header>
+ <main>
+ 	<section class="form-block">
+ 		
+	 		<div class="form-block-greeting">
+	 			<div class="main-wrap wrap form-block-wrap">
+		 			<h1>welcome</h1>
+		 			<p>College students can publish their couse notes, study together, build their acdemic reputations....</p>
+	 			</div>
+	 		</div>
+			<div class="form">
+				<div class="form-wrap">
+					<div class="form-input">
+						<h2>Get Started</h2>
+						<label><input  type="text" value="Select Course"></label>
+						<input  type="text" placeholder="First Name">
+						<input  type="text" placeholder="Last Name">
+						<input  type="tel" placeholder="Phone">
+						<input  class="form-input-Adress" type="text" placeholder="Adress">
+						<input  type="text" placeholder="Zip">
+						<input  type="email" placeholder="Email">
+						<input  class="form-imput__submit" type="submit" value="Submit">
+					</div>
+				</div>
+				<div class="form-description">
+					<div class="wrap">
+			 			<h3>abc university</h3>
+			 			<p>In 1865, Ezra Cornell founded an institution "where any person can find instruction in any study." From the beginning, all students were welcome, regardless of race, gender, ideology, or socioeconomic status.</p>
+						<p>Today, we invite you to join our talented and diverse students and accessible faculty who, together, form a living and learning community unmatched in its breadth of opportunities.</p>
+		 			</div>
+		 		</div>
+			</div>
+ 	</section>
+ 	<section class="advantages-block">
+ 		<div class="advantages-block-wrap">
+ 			<h2>why we are best</h2>
+			<div class="advantages-block-inn first-block">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/goit/bild.jpg">
+				<h3>Quality environment</h3>
+				<p>We guide our students to find their own academic paths by helping them to reason, analyze and understand in unprecedented ways. The undergraduate experience at Duke is characterized by a spirit of academic energy, exploration and challenge.</p>
+				<a href=""><span>view</span></a>	
+			</div>
+			<div class="advantages-block-inn">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/goit/women.jpg">
+				<h3>Quality programs</h3>
+				<p>We are a research university with resources to connect undergraduate education to the processes of inquiry and discovery. Duke students explore ideas by collaborating directly with our exceptional faculty in a program of study that focuses on interdisciplinary exploration.</p>
+				<a href=""><span>view</span></a>
+			</div>	
+			<div class="advantages-block-inn">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/goit/men.jpg">
+				<h3>Excellent teachers</h3>
+				<p>A student's education doesn't end at the classroom door. We provide an incredible variety of opportunities for students to enhance and supplement in-class learning with hands-on experiences, service in the community—both locally and globally—and development of leadership skills.</p>
+				<a href=""><span>view</span></a>
+			</div>	
+ 		</div>
+ 	</section>
+ 	<section class="comments">
+	 		<div class="comments-content">
+		 		<div class="comments-wrap wrap">
+		 			<h2>What Students Says About Our University</h2>
+		 			<div class="comments-content-button">
+			 			<div class="comments-content-button-inn"><img src="<?php echo get_template_directory_uri(); ?>/img/goit/arrow--l.png"></div>
+			 			<div class="comments-content-button-inn right-arrow-button"><img src="<?php echo get_template_directory_uri(); ?>/img/goit/arrow--l.png"></div>
+		 			</div>
+		 			<div class="comments-content-inn">
+		 				<p>We guide our students to find their own academic paths by helping them to reason, analyze and understand in unprecedented ways. The undergraduate experience at Duke is characterized by a spirit of academic energy, exploration and challenge.</p>
+		 				<span>&mdash; Amanda Persch</span>
+		 			</div>
+		 		</div>	
+	 		</div>
+	 		<div class="comments-act">
+		 		<div class="comments-wrap wrap">
+		 			<div class="comments-act__bottom">
+		 				<p>Click Here Now to Get Started</p>
+		 				<img src="<?php echo get_template_directory_uri(); ?>/img/goit/arrow-r.png">
+		 			</div>
+		 			<div class="comments-act-img"><img src="<?php echo get_template_directory_uri(); ?>/img/boy.png"></div>
+		 		</div>
+ 			</div>
+ 	</section>
+ </main>
+
+<footer>
+	<div class="header-wrap wrap footer-wrap">
+		<div class="logo-block">
+		 	<div class="logo"><img src="<?php echo get_template_directory_uri(); ?>/img/logo1.png"></div>
+		 	<p>abc university</p>
+		 </div>
+	 	<div class="social footer-social">
+			<div class="social-title">connect</div>
+			<div class="social-logo footer-social-logo">
+		 		<div class="social-img"><img src="<?php echo get_template_directory_uri(); ?>/img/facebook.png" alt=""></div>
+		 		<div class="social-img"><img src="<?php echo get_template_directory_uri(); ?>/img/gplus.png"  alt=""></div>
+		 		<div class="social-img"><img src="<?php echo get_template_directory_uri(); ?>/img/tw.png"  alt=""></div>
+	 		</div>
+	 	</div>
+	 	<p class="footer-info">© 2014 by ABC UNIVERSITY OF LIFE. Proudly created with unbounce.com. Design by Design-What.com</p>
+	 </div>
+</footer>
+
+
+</body>
